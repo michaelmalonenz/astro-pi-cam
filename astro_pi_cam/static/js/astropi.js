3d91ae77e16bee5c023f2e2e0367fe504157a2c2
Michael Malone <michael.j.b.malone@gmail.com>
2020-05-19 19:37:34 +1200

    Add the query params for the preview

--- astro_pi_cam/static/js/astropi.js
+++ astro_pi_cam/static/js/astropi.js
@@ -1,10 +1,15 @@
+/* global $ */
 function showPreview () {
-
-    $.ajax({
-        method: 'POST',
-        url: '/camera/take_image'
-    }).done(function (data) {
-        $('.modal #preview').attr('src', `data:image/jpeg;base64,${data}`)
-        $('.modal').modal()
-    })
+  const fieldNames = ['resolution', 'iso', 'meter_mode', 'exposure_mode', 'shutter_speed']
+  let queryParams = []
+  for (let field of fieldNames) {
+    queryParams.push(field + '=' + $(`input[name=${field}]`).val())
+  }
+  $.ajax({
+    method: 'POST',
+    url: `/camera/take_image?${queryParams.join('&')}`
+  }).done(function (data) {
+    $('.modal #preview').attr('src', `data:image/jpeg;base64,${data}`)
+    $('.modal').modal()
+  })
 }
