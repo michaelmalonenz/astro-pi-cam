6a4924218a92432ca4dcde5c66adc8bdcc7cfa53
Michael Malone <michael.j.b.malone@gmail.com>
2020-05-29 22:05:52 +1200

    Theoretically deployable prod-ready webserver

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+systemctl link "$DIR"/astropicam.service
+systemctl enable astropicam.service
+systemctl start --no-block astropicam.service
+
+ln -s "$DIR"/astropicam_nginx.conf /etc/nginx/sites_enabled/astropicam.conf
+
+systemctl restart nginx
